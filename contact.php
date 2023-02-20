c6bbbe59233969bb62b387755b08afd90c39cea5
guru33333 <gobiggbi@gmail.com>
2023-02-20 07:33:20 +0530

    Push

---
+++ contact.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="en">
+<head itemscope itemtype="http://www.schema.org/website">
+	<title>Sakthivel Offset Printers </title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta property="og:title" content="Sakthivel Offset Printers">
+	<meta property="og:type" content="website">	
+	<meta property="og:url" content="https://.com">
+	<meta property="og:image" content="https://.com/images/android-icon-192x192.png">
+	<meta property="og:site_name" content="Sakthivel Offset Printers">
+	<meta name="keywords" content="">
+	<meta property="og:description" name="description" content="">
+	<meta name="robots" content="all">
+	<meta name="revisit-after" content="10 Days">
+	<meta name="copyright" content="Sakthivel Offset Printers">
+	<meta name="language" content="English">
+	<meta name="distribution" content="Global">
+	<meta name="msapplication-TileImage" content="images/ms-icon-144x144.png">
+  	<link rel="icon" type="image/png" sizes="96x96" href="images/favicon-96x96.png">
+  	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-icon-72x72.png">
+  	<link rel="icon" sizes="192x192"  href="images/android-icon-192x192.png">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/wow.js"></script>
+</head>
+<body itemscope itemtype="http://schema.org/WebPage">
+<!--header-->
+<div class="head-bg py-3">
+	<div class="container-fluid px-lg-4">
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="top-bar-info">
+					<ul class="fullpad">
+						<li><a href="tel:+917598755726" class="roboto text-dark"><i class="fa fa-phone text-clr1 pr-1"></i> (+91) 75987 55726</a></li>
+						<li><a href="mailto:sakthiveloffsetprinters@gmail.com" class="roboto text-dark"><i class="fa fa-envelope text-clr1 pr-1"></i> sakthiveloffsetprinters@gmail.com</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-6 text-lg-right d-none d-lg-block">
+				<a href="#" class="quote-btn"><i class="fa fa-envelope text-white pr-1"></i> Get Quote</a>
+			</div>
+		</div>
+	</div>
+</div>	
+<!--header-->
+<!--Desktop Nav-->
+<nav class="navbar navbar-expand-lg navbar-light navfont fnt-500 bg-white shadow sticky-top main d-none d-lg-block">
+	<div class="container-fluid px-lg-4">
+		<a href="index.html">
+			<img itemprop="image" src="images/sakthiveloffsetlogo.png" class="img-fluid logo" alt="Sakthivel Offset" title="Sakthivel Offset">
+		</a>
+		<div id="myNavbar" class="collapse navbar-collapse">
+			<ul class="navbar-nav ml-auto text-center text-white">
+                <li class="nav-item px-2">
+					<a class="nav-link" href="index.html">HOME</a>
+				</li>
+				<li class="nav-item px-2">
+					<a class="nav-link" href="about.html">ABOUT</a>
+				</li>
+				<li class="nav-item dropdown px-3 pos">
+					<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" id="navbarDropdown" role="button">PRODUCTS</a>
+					<ul class="dropdown-menu megamenu">
+						<div class="row">
+							<li class="col-md-4 pt-4 align-self-center">
+								<div class="align-self-center">
+									<img itemprop="image" src="images/sakthiveloffsetlogo.png" class="img-fluid inner-logo" alt="Sakthivel Offset" title="Sakthivel Offset">
+								</div>
+							</li>
+							<li class="col-md-4 pt-4">
+								<ul class="fullpad links">
+									<li><a href="popcornbox.html">Popcorn Cover &amp; Boxes</a></li>
+									<li><a href="calendars.html">Calendars</a></li>
+									<li><a href="hospitalreportbag.html">Hospital Report Bags</a></li>
+									<li><a href="ctscanbag.html">CT Scan Bags</a></li>
+									<li><a href="paperbags.html">Paper Bags</a></li>
+									<li><a href="noticeprinting.html">Notice Printing</a></li>
+									<li><a href="weddingcard.html">Wedding Cards</a></li>
+								</ul>
+							</li>
+							<li class="col-md-4 pt-4">
+								<ul class="fullpad links">
+									<li><a href="cakesweetbox.html">Cake &amp; Sweet Box</a></li>
+									<li><a href="frenchfriesbox.html">French Fries Box</a></li>
+									<li><a href="notebooks.html">Notebooks </a></li>
+									<li><a href="examsheets.html">Exam Sheets</a></li>
+									<li><a href="wallposters.html">Wall Posters</a></li>
+									<li><a href="stickerslabels.html">Stickers &amp; Labels</a></li>
+									<li><a href="otherprinting.html">Other Printing</a></li>
+								</ul>
+							</li>
+						</div>
+					</ul>
+				</li>
+				<li class="nav-item px-2">
+					<a class="nav-link" href="infrastructure.html">INFRASTRUCTURE</a>
+				</li>
+				<li class="nav-item px-2">
+					<a class="nav-link active" href="contact.php">CONTACT</a>
+				</li>
+			</ul>
+		</div> 	
+	</div>	
+</nav>
+<!--Desktop Nav-->
+<!--Mobile Nav-->
+<nav class="navbar navbar-expand-lg navbar-light sticky-top shadow bg-white navfont fnt-500 d-lg-none">
+	<div class="container-fluid px-lg-4">
+		<a href="index.html">
+			<img itemprop="image" src="images/sakthiveloffsetlogo.png" class="img-fluid logo" alt="Sakthivel Offset" title="Sakthivel Offset">
+		</a>
+		<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#myNavbar" id="togals">
+			<span class="fa fa-bars text-dark"> Menu </span>
+		</button>
+		<div id="myNavbar" class="collapse navbar-collapse">
+			<ul class="navbar-nav ml-auto text-center text-white">
+                <li class="nav-item px-2">
+					<a class="nav-link" href="index.html">HOME</a>
+				</li>
+				<li class="nav-item px-2">
+					<a class="nav-link" href="about.html">ABOUT</a>
+				</li>
+				<li class="nav-item px-2 dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						PRODUCTS</a>
+					<ul class="dropdown-menu text-center" aria-labelledby="navbarDropdownMenuLink">
+						<li><a class="dropdown-item" href="popcornbox.html">Popcorn Cover &amp; Boxes</a></li>
+						<li><a class="dropdown-item" href="calendars.html">Calendars</a></li>
+						<li><a class="dropdown-item" href="hospitalreportbag.html">Hospital Report Bags</a></li>
+						<li><a class="dropdown-item" href="ctscanbag.html">CT Scan Bags</a></li>
+						<li><a class="dropdown-item" href="paperbags.html">Paper Bags</a></li>
+						<li><a class="dropdown-item" href="noticeprinting.html">Notice Printing</a></li>
+						<li><a class="dropdown-item" href="weddingcard.html">Wedding Cards</a></li>
+						<li><a class="dropdown-item" href="cakesweetbox.html">Cake &amp; Sweet Box</a></li>
+						<li><a class="dropdown-item" href="frenchfriesbox.html">French Fries Box</a></li>
+						<li><a class="dropdown-item" href="notebooks.html">Notebooks </a></li>
+						<li><a class="dropdown-item" href="examsheets.html">Exam Sheets</a></li>
+						<li><a class="dropdown-item" href="wallposters.html">Wall Posters</a></li>
+						<li><a class="dropdown-item" href="stickerslabels.html">Stickers &amp; Labels</a></li>
+						<li><a class="dropdown-item" href="otherprinting.html">Other Printing</a></li>
+					</ul>
+				</li>
+				<li class="nav-item px-2">
+					<a class="nav-link" href="infrastructure.html">INFRASTRUCTURE</a>
+				</li>
+				<li class="nav-item px-2">
+					<a class="nav-link active" href="contact.php">CONTACT</a>
+				</li>
+			</ul>
+		</div> 	
+	</div>	
+</nav>
+<!--Mobile Nav-->
+<!--Banner-->
+<div class="contactbg">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="s-banner-text ">
+              	<h1 class="banner-font roboto">Contact Us</h1>
+                <div class="roboto">If you’d like to talk to us about a project, or anything else, we’d love to hear from you.</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--Banner-->
+<div class="bg-light">
+    <div class="container pad">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-12">
+                <h1 class="roboto fnt-500 ">Enquiry</h1>
+      		    <div class="section-heading-line-left"></div>
+                <form class="py-2 contact-form" action="contact.php" method="POST">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-12 pt-3">
+                            <label class="roboto">Name</label>
+                            <input type="text" class="form-control roboto" id="name" name="name" placeholder="Your Name" required>
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-12 pt-3">
+                            <label class="roboto">Mobile Number</label>
+                            <input type="text" class="form-control roboto" id="number" name="number"  pattern="[0-9]{10,15}" placeholder="Your Mobile Number " required>
+                        </div>	
+                        <div class="col-lg-12 col-md-12 col-12 pt-3">
+                            <label class="roboto">Email</label>
+                            <input type="email" class="form-control roboto" id="email" name="email" placeholder="Your Email" required>
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-12 pt-3">
+                             <label class="roboto">Message</label>    
+                            <textarea class="form-control formheight roboto" id="message" name="message" placeholder="Your Message Here" required></textarea>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-12 pt-3">
+                            <input class="roboto  w-100" type="submit" value="SUBMIT" id="send" name="send">
+                        </div>
+                    </div>	   
+				</form> 
+            </div>
+            <div class="col-lg-6 col-md-6 col-12 pt-lg-5 pl-lg-5 pt-4">
+                <div class="d-flex pb-3">
+                    <div class="cnt-icons pr-3">
+                        <i class="fa fa-location-arrow"></i>
+                    </div>
+                    <div class="icon-info">
+                        <h3 class="roboto heading4">Address</h3>
+                        <div class="roboto smallfnt">116/2, Police Station Road, <br> Rathinam Nagar, (Near Sivakasi Municipality)<br> Sivakasi - 626 123</div>
+                    </div>
+                </div>
+                <hr>
+                <div class="d-flex pb-3">
+                    <div class="cnt-icons pr-3">
+                        <i class="fa fa-phone"></i>
+                    </div>
+                    <div class="icon-info">
+                        <h3 class="roboto heading4">Call Us</h3>
+                        <div class="roboto smallfnt">(+91) 75987 55726</div>
+                    </div>
+                </div>
+                <hr>
+                <div class="d-flex pb-3">
+                    <div class="cnt-icons pr-3">
+                        <i class="fa fa-envelope"></i>
+                    </div>
+                    <div class="icon-info">
+                        <h3 class="roboto heading4">Email</h3>
+                        <div class="roboto smallfnt">sakthiveloffsetprinters@gmail.com</div>
+                    </div>
+                </div>
+                <hr>
+                <div class="d-flex pb-3">
+                    <div class="cnt-icons pr-3">
+                        <i class="fa fa-clock-o"></i>
+                    </div>
+                    <div class="icon-info">
+                        <h3 class="roboto heading4">Working Hours</h3>
+                        <div class="roboto smallfnt">Mon-Sat 10:00 - 20:00 IST</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="bg-dark">
+	<div class="container">
+		<div class="row">	
+			<div class="col-lg-12 footbox text-center py-2">
+				<div class="roboto text-white smallfnt">Copyright &copy; 2022, Sakthivel Offset Printerss. <span class="wrdbrk">All rights reserved </span> &nbsp; &nbsp;<span>Developed by <a href="#" target="_blank" class="wrdbrk text-white">GoBigg</a></span></div>
+			</div>
+		</div>
+	</div>
+</div>
+<script>
+    $('.nav-link').addClass('text-white');
+    $(window).on("scroll", function() {
+        if($(window).scrollTop()) {
+            $('.mynav').addClass('black');
+		    $('.nav-link').removeClass('text-white');
+		    $('.mynav').removeClass('black1');
+        }
+        else {
+            $('.mynav').addClass('black1');
+            $('.nav-link').addClass('text-white');
+		    $('.mynav').removeClass('black');
+        }
+      })
+</script>
+<script>
+	new WOW().init();
+
+	$(document).ready(function(){
+    $('ul.navbar-nav li.dropdown').hover(function() {
+      $(this).find('.dropdown-menu').stop(true, true).delay(200).slideDown(400);
+    }, function() {
+
+
+      $(this).find('.dropdown-menu').stop(true, true).delay(200).slideUp(400);
+    });  
+});
+</script>
+</body>
+</html>
